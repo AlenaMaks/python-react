cc40b27fe53e7850917c9abfaaa45517a5e4f1b2
AlenaMaks <deia1704092@mail.ru>
2026-03-17 18:16:02 +0300

    доделка

--- work4/4.py
+++ work4/4.py
@@ -1,2 +1,19 @@
 # 4 задание
 
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"\nФункция {func.__name__} вызвана с аргументами: ")
+        print(f"Позиционные элементы: {args}")
+        print(f"Именованные элементы: {kwargs}")
+        return func(*args, **kwargs) # без использования переменной result
+    return wrapper
+
+# основная функция обёрнутая в декоратор
+@decorator
+def rectangle_S(a, b):
+    return a * b
+
+a = int(input("Первая сторона: "))
+b = int(input("Вторая сторона: "))
+
+print(f"Площадь прямоугольника: {rectangle_S(a, b)}")
