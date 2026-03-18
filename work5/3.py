31da2c71e34a9430cd761bfa4c5dcf9bdcb05fe9
AlenaMaks <deia1704092@mail.ru>
2026-03-18 18:03:10 +0300

    успелааа

--- work5/3.py
+++ work5/3.py
@@ -1,1 +1,26 @@
 # 3 задание
+from faker import Faker
+from string import ascii_letters, digits, punctuation
+from random import randint, choices
+from json import dump, load
+
+chars = ascii_letters + digits + punctuation
+
+name = Faker().name() # для генерации нормальных имён
+
+information = {
+    "name": name, 
+    "age": randint(18, 90),
+    "email": name.split()[0].lower()+'.'+name.split()[1].lower()+'@mail.com',
+    "password": ''.join(choices(chars, k = 12))
+}
+
+filename = input('Введите имя файла для сохранения json: ')
+
+with open(filename, 'w') as file:
+    dump(information, file)
+    
+with open(filename, 'r') as file:
+    result = load(file)
+    
+print(result, type(result))
--- work5/4.py
+++ work5/4.py
@@ -1,1 +1,16 @@
 # 4 задание
+from array import array
+from datetime import date, timedelta
+from random import randint
+
+days = array('i') # для хранения разницы в днях
+
+now = date.today()
+all_dates = []
+for i in range(10):
+    all_dates.append(now - timedelta(randint(0, 365*5)))
+for i in range(len(all_dates)-1):
+    days.append(abs(all_dates[i] - all_dates[i+1]).days)
+
+for i in range(1, len(all_dates)):
+    print(f"{i}. Разница между {all_dates[i-1]} и {all_dates[i]}: {days[i-1]} дней")
