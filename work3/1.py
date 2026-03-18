f8df8d49b63b5594c2441ad5f9b5f3041d7a6cfd
AlenaMaks <deia1704092@mail.ru>
2026-03-18 18:33:47 +0300

    ошибка была

--- work3/1.py
+++ work3/1.py
@@ -10,13 +10,13 @@ def func_mn(lst: list[str], x: int = 2) -> list[int]:
 lst = input("Введите список чисел через пробел: ").split()
 x = input("Введите множитель (по умолчанию 2): ")
 
-# случай когда ничего не ввели в множитель или ввели не число
+# случай когда ничего не ввели множитель или ввели не число
 if not(x.lstrip('-').isdigit()):
     print(f"Результат (функция): {func_mn(lst)}")
     func_mn_l = list(map(lambda num, x = 2: int(num) * int(x), lst))
     print(f"Результат (лямбда-функция): {func_mn_l}")
 else:
     # когда корректный ввод множителя
-    print(f"Результат (функция): {func_mn(lst, x)}")
-    func_mn_l = list(map(lambda num, x: int(num) * int(x), lst))
+    print(f"Результат (функция): {func_mn(lst, int(x))}")
+    func_mn_l = list(map(lambda num: int(num) * int(x), lst))
     print(f"Результат (лямбда-функция): {func_mn_l}")
