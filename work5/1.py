0996d27fe4f21f19d845985f391672bcba288ede
AlenaMaks <deia1704092@mail.ru>
2026-03-18 13:59:59 +0300

    ползём

--- work5/1.py
+++ work5/1.py
@@ -1,1 +1,22 @@
 # 1 задание
+from random import choice
+from pathlib import Path
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+lst_filenames = []
+for name in range(10):
+    filename = ''
+    for i in range(8):
+        chr = choice(chars)
+        filename += chr
+    filename += '.txt'
+    lst_filenames.append(filename)
+print(lst_filenames)
+
+directory = Path(input('Введите название директории: ')) # Path преобразовывает в <class 'pathlib.WindowsPath'>
+directory.mkdir(exist_ok=True) # если папка уже есть, не будет ошибки
+
+for filename in lst_filenames:
+    full_path = directory / filename
+    full_path.touch()
+    print(full_path.absolute())
--- work5/2.py
+++ work5/2.py
@@ -1,1 +1,12 @@
 # 2 задание
+from random import randint
+from statistics import mean, median, pstdev
+from math import sqrt
+
+lst_numbers = [randint(1, 100) for i in range(100)]
+avg = round(mean(lst_numbers), 2)
+mediana = round(median(lst_numbers), 2)
+deviation = round(pstdev(lst_numbers), 2) # можно stdev для выборки (разные формулы)
+root = round(sqrt(sum(lst_numbers)), 2)
+
+print(f"Среднее: {avg}, Медиана: {mediana}, Стандартное отклонение: {deviation}, Корень из суммы: {root}")
