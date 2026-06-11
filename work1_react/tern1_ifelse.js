928d41387eb75b4356c239b2c3834bcb8242ab26
AlenaMaks <deia1704092@mail.ru>
2026-06-11 22:35:53 +0300

    1_react_исправлениеошибок

--- work1_react/tern1_ifelse.js
+++ work1_react/tern1_ifelse.js
@@ -1,35 +1,55 @@
-let a = Math.floor(Math.random() * 100); // ответ не 5, если a = 0, 1, 2
+let a = Math.floor(Math.random() * 100); 
 let step1
 let step2
 let step3
 
-// (a > 10 ? a : a * 2)
 if (a > 10) {
     step1 = a
 }
 else step1 = a*2
 if (step1 > 5) {
     step2 = (2 * a) + 1
+    console.log("Значение a: ", a)
+    console.log("Ответ: ", step2)
 }
-// (a < 3 ? 1 : 2 * (a - 2))
 else {
     if (a < 3) {
         step2 = 1
     }
     else step2 = 2 * (a - 2)
-}
-if (step2 > 4) {
-    step3 = 5
-}
-// (a % 2 == 0 ? 6 : 7)
-else {
-    if (a % 2 == 0) {
-        step3 = 6
+    if (step2 > 4) {
+        step3 = 5
     }
-    else step3 = 7
+    else {
+        if (a % 2 == 0) {
+        step3 = 6
+        }
+        else step3 = 7
+    }
+    console.log("Значение a: ", a)
+    console.log("Ответ: ", step3)
 }
+// БЫЛО ошибка была в неправильной логике if else (step2 > 4)
+// и то что выводила обязательно step3, хотя вычисление могло закончится на step2
+// else {
+//     if (a < 3) {
+//         step2 = 1
+//     }
+//     else step2 = 2 * (a - 2)
+// }
+// if (step2 > 4) {
+//     step3 = 5
+// }
+// else {
+//     if (a % 2 == 0) {
+//         step3 = 6
+//     }
+//     else step3 = 7
+// }
+// console.log("Значение a: ", a)
+// console.log("Ответ: ", step3)
 
-console.log("Значение a: ", a)
-console.log("Ответ: ", step3)
-
-// (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
+// исходник (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
+// логика
+// (a > 10 ? a : a * 2) если > 5 тогда (2 * a) + 1 иначе (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7)
+// (a < 3 ? 1 : 2 * (a - 2)) > 4 тогда 5 иначе (a % 2 == 0 ? 6 : 7)
--- work1_react/tern1_switch.js
+++ work1_react/tern1_switch.js
@@ -3,7 +3,6 @@ let step1
 let step2
 let step3
 
-// (a > 10 ? a : a * 2)
 switch (true) {
     case (a > 10):
         step1 = a
@@ -15,28 +14,34 @@ switch (true) {
 switch (true) {
     case (step1 > 5):
         step2 = (2 * a) + 1
-        break
-    // (a < 3 ? 1 : 2 * (a - 2))
-    case (a < 3):
-        step2 = 1
+        console.log("Значение a: ", a)
+        console.log("Ответ: ", step2)
         break
     default:
-        step2 = 2 * (a - 2)
+        switch (true) {
+            case (a < 3):
+                step2 = 1
+                break
+            default:
+                step2 = 2 * (a - 2)
+        }
+        switch (true) {
+            case (step2 > 4):
+                step3 = 5
+                break
+            default: 
+            switch (true) {
+                case (a % 2 == 0):
+                step3 = 6
+                break
+                default:
+                step3 = 7
+            }
+        }
+        console.log("Значение a: ", a)
+        console.log("Ответ: ", step3)
 }
 
-switch (true) {
-    case (step2 > 4):
-        step3 = 5
-        break
-    //(a % 2 == 0 ? 6 : 7)
-    case (a % 2 == 0):
-        step3 = 6
-        break
-    default:
-        step3 = 7
-}
-
-console.log("Значение a: ", a)
-console.log("Ответ: ", step3)
-
-// (a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
+// аналогичная ошибка с if else, пересмотренная логика та же
+// (a > 10 ? a : a * 2) если > 5 тогда (2 * a) + 1 иначе (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7)
+// (a < 3 ? 1 : 2 * (a - 2)) > 4 тогда 5 иначе (a % 2 == 0 ? 6 : 7)
